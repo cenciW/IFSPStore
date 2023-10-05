7fc55412170e1b72efaa3d162a45e369ceed92dd
cenciW <ze.ccenci@gmail.com>
2023-10-05 10:02:43 -0300

    Teste + Usuario + Cidade + Grupo

--- IFSPStore.Test/UnitTestRepository.cs
+++ IFSPStore.Test/UnitTestRepository.cs
@@ -12,15 +12,98 @@ namespace IFSPStore.Test
     [TestClass]
     public class UnitTestRepository
     {
+        #region Test Usuario
         [TestMethod]
         public void TestUsuario()
         {
+            using (var context = new MyDbContext()){
+                var usuario = new Usuario("Murilo Varges",
+                            "murilo", "semsenha", "email",
+                            DateTime.UtcNow.ToLocalTime(), DateTime.UtcNow.ToLocalTime(), true);
 
+                context.Usuario.Add(usuario);
+                context.SaveChanges();
+            };
         }
+
+        [TestMethod]
+        public void TestSelectUsuario()
+        {
+            using (var context = new MyDbContext())
+            {
+                foreach (var usuario in context.Usuario)
+                {
+                    {
+                        Console.WriteLine(JsonSerializer.Serialize(usuario));
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Teste Cidade
+        [TestMethod]
+        public void TestCidade()
+        {
+            using (var context = new MyDbContext())
+            {
+                var cidade = new Cidade("Aracatuba", "SP");
+
+                context.Cidade.Add(cidade);
+                context.SaveChanges();
+            };
+            
+        }
+
+        [TestMethod]
+        public void TestSelectCidade()
+        {
+            using (var context = new MyDbContext())
+            {
+                foreach (var cidade in context.Cidade)
+                {
+                    {
+                        Console.WriteLine(JsonSerializer.Serialize(cidade));
+                    }
+                }
+            };
+        }
+        #endregion
+
+        #region Test Grupo
+        [TestMethod]
+        public void TestGrupo()
+        {
+            using (var context = new MyDbContext())
+            {
+                var grupo = new Grupo("Yakissoba");
+
+                context.Grupo.Add(grupo);
+                context.SaveChanges();
+            };
+        }
+
+        [TestMethod]
+        public void TesteSelectGrupo()
+        {
+            using (var context = new MyDbContext())
+            {
+                foreach (var grupo in context.Grupo)
+                {
+                    {
+                        Console.WriteLine(JsonSerializer.Serialize(grupo));
+                    }
+                }
+            }
+        }
+        #endregion
     }
 
     public class MyDbContext : DbContext{
         public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Cidade> Cidade{ get; set; }
+        public DbSet<Grupo> Grupo { get; set; }
+        public DbSet<Produto> Produto { get; set; }
         public MyDbContext()
         {
             //Força a criação do banco de dados;
